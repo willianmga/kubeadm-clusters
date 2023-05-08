72c6c62e1baf5ff9b82873e50242c423b96583fb
Willian Azevedo <willian.bodnariuc@liferay.com>
2023-05-08 19:46:57 +0100

    Create setup-cluster.sh

---
+++ setup-cluster.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Optimized for Kubernetes v1.17.1
+
+# WeaveNet CNI
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+
+# NGINX Ingress Controller v1.7.1 
+https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.7.1/deploy/static/provider/cloud/deploy.yaml
+
