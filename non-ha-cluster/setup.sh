d4a8659a031a94c99cb7ae37337dc2e2d29728e2
Willian Azevedo <willian.bodnariuc@liferay.com>
2023-05-31 21:15:45 +0100

    Update setup.sh

--- non-ha-cluster/setup.sh
+++ non-ha-cluster/setup.sh
@@ -70,7 +70,7 @@ install_kubernetes_tools() {
 
     sudo apt-get update
     sudo apt-get install -y apt-transport-https ca-certificates curl
-    sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+    curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
     echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
     sudo apt-get update
